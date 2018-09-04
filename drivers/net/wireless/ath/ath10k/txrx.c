75ba14b4824144548d661f3e203ebb8a624fd0c7
Erik Stromdahl <erik.stromdahl@gmail.com>
2018-09-04 15:05:23 +0300

    ath10k: DMA related fixes for high latency devices
    Several DMA related functions (such as the dma_map_xxx functions)
    are not used with high latency devices and don't need to be invoked
    in this case.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/txrx.c
+++ drivers/net/wireless/ath/ath10k/txrx.c
@@ -93,7 +93,8 @@ int ath10k_txrx_tx_unref(struct ath10k_htt *htt,
 	ath10k_htt_tx_dec_pending(htt);
 	spin_unlock_bh(&htt->tx_lock);
 
-	dma_unmap_single(dev, skb_cb->paddr, msdu->len, DMA_TO_DEVICE);
+	if (ar->dev_type != ATH10K_DEV_TYPE_HL)
+		dma_unmap_single(dev, skb_cb->paddr, msdu->len, DMA_TO_DEVICE);
 
 	ath10k_report_offchan_tx(htt->ar, msdu);
 
