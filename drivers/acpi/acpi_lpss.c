8ed118ba9462db10ed59154c2f448adc90ce769d
Linus Torvalds <torvalds@linux-foundation.org>
2018-10-23 10:33:16 +0100

    Merge tag 'acpi-4.20-rc1' of git://git.kernel.org/pub/scm/linux/kernel/git/rafael/linux-pm
    Pull ACPI updates from Rafael Wysocki:
     "These fix ACPICA issues related to the handling of module-level AML,
      fix an ordering issue during ACPI initialization, update ACPICA to
      upstream revision 20181003 (including fixes mostly), fix issues with
      system-wide suspend/resume related to the ACPI driver for Intel SoCs
      (LPSS), fix device enumeration issues on boards with Dollar Cove or
      Whiskey Cove Intel PMICs, prevent ACPICA from calling ktime_get() in
      unsuitable conditions, update a few drivers and clean up some code in
      several places.
    
      Specifics:
    
       - Fix ACPICA issues related to the handling of module-level AML and
         make the ACPI initialization code parse ECDT before loading the
         definition block tables (Erik Schmauss).
    
       - Update ACPICA to upstream revision 20181003 including fixes related
         to the ill-defined "generic serial bus" and the handling of the
         _REG object (Bob Moore).
    
       - Fix some issues with system-wide suspend/resume on Intel BYT/CHT
         related to the handling of I2C controllers in the ACPI LPSS driver
         for Intel SoCs (Hans de Goede).
    
       - Modify the ACPI namespace scanning code to enumerate INT33FE HID
         devices as platform devices with I2C resources to avoid device
         enumeration problems on boards with Dollar Cove or Whiskey Cove
         Intel PMICs (Hans de Goede).
    
       - Prevent ACPICA from using ktime_get() during early resume from
         system-wide suspend before resuming the timekeeping which generally
         is unsafe and triggers a warning from the timekeeping code (Bart
         Van Assche).
    
       - Add low-level real time clock support to the ACPI Time and Aalarm
         Device (TAD) driver (Rafael Wysocki).
    
       - Fix the ACPI SBS driver to avoid GPE storms on MacBook Pro and
         Oopses when removing modules (Ronald Tschalär).
    
       - Fix the ACPI PPTT parsing code to handle architecturally unknown
         cache types properly (Jeffrey Hugo).
    
       - Fix initialization issue in the ACPI processor driver (Dou Liyang).
    
       - Clean up the code in several places (Andy Shevchenko, Bartlomiej
         Zolnierkiewicz, David Arcari, zhong jiang)"
    
    * tag 'acpi-4.20-rc1' of git://git.kernel.org/pub/scm/linux/kernel/git/rafael/linux-pm: (33 commits)
      ACPI / scan: Create platform device for INT33FE ACPI nodes
      ACPI / OSL: Use 'jiffies' as the time bassis for acpi_os_get_timer()
      ACPI: probe ECDT before loading AML tables regardless of module-level code flag
      ACPICA: Remove acpi_gbl_group_module_level_code and only use acpi_gbl_execute_tables_as_methods instead
      ACPICA: AML Parser: fix parse loop to correctly skip erroneous extended opcodes
      ACPICA: AML interpreter: add region addresses in global list during initialization
      ACPI: TAD: Add low-level support for real time capability
      ACPI: remove redundant 'default n' from Kconfig
      ACPI / SBS: Fix rare oops when removing modules
      ACPI / SBS: Fix GPE storm on recent MacBookPro's
      ACPI/PPTT: Handle architecturally unknown cache types
      drivers: base: cacheinfo: Do not populate sysfs for unknown cache types
      ACPICA: Update version to 20181003
      ACPICA: Never run _REG on system_memory and system_IO
      ACPICA: Split large interpreter file
      ACPICA: Update for field unit access
      ACPICA: Rename some of the Field Attribute defines
      ACPICA: Update for generic_serial_bus and attrib_raw_process_bytes protocol
      ACPI / processor: Fix the return value of acpi_processor_ids_walk()
      ACPI / LPSS: Resume BYT/CHT I2C controllers from resume_noirq
      ...
    
    Signed-off-by: skye-pa1n <sonicgames772@gmail.com>

--- drivers/acpi/acpi_lpss.c
+++ drivers/acpi/acpi_lpss.c
@@ -478,26 +478,35 @@ struct lpss_device_links {
  */
 static const struct lpss_device_links lpss_device_links[] = {
 	{"808622C1", "7", "80860F14", "3", DL_FLAG_PM_RUNTIME},
+	{"808622C1", "7", "LNXVIDEO", NULL, DL_FLAG_PM_RUNTIME},
+	{"80860F41", "5", "LNXVIDEO", NULL, DL_FLAG_PM_RUNTIME},
 };
 
-static bool hid_uid_match(const char *hid1, const char *uid1,
+static bool hid_uid_match(struct acpi_device *adev,
 			  const char *hid2, const char *uid2)
 {
-	return !strcmp(hid1, hid2) && uid1 && uid2 && !strcmp(uid1, uid2);
+	const char *hid1 = acpi_device_hid(adev);
+	const char *uid1 = acpi_device_uid(adev);
+
+	if (strcmp(hid1, hid2))
+		return false;
+
+	if (!uid2)
+		return true;
+
+	return uid1 && !strcmp(uid1, uid2);
 }
 
 static bool acpi_lpss_is_supplier(struct acpi_device *adev,
 				  const struct lpss_device_links *link)
 {
-	return hid_uid_match(acpi_device_hid(adev), acpi_device_uid(adev),
-			     link->supplier_hid, link->supplier_uid);
+	return hid_uid_match(adev, link->supplier_hid, link->supplier_uid);
 }
 
 static bool acpi_lpss_is_consumer(struct acpi_device *adev,
 				  const struct lpss_device_links *link)
 {
-	return hid_uid_match(acpi_device_hid(adev), acpi_device_uid(adev),
-			     link->consumer_hid, link->consumer_uid);
+	return hid_uid_match(adev, link->consumer_hid, link->consumer_uid);
 }
 
 struct hid_uid {
@@ -513,8 +522,7 @@ static int match_hid_uid(struct device *dev, void *data)
 	if (!adev)
 		return 0;
 
-	return hid_uid_match(acpi_device_hid(adev), acpi_device_uid(adev),
-			     id->hid, id->uid);
+	return hid_uid_match(adev, id->hid, id->uid);
 }
 
 static struct device *acpi_lpss_find_device(const char *hid, const char *uid)
