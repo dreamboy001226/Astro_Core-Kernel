5bf98fddb87c57402c3f01003cd10f7e6bbaabb6
skye-tachyon <sonicgames772@gmail.com>
2026-03-29 18:52:30 -0300

    [DNM] drivers: cpuidle: hacks
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- drivers/cpuidle/poll_state.c
+++ drivers/cpuidle/poll_state.c
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: GPL-2.0-only
 /*
  * poll_state.c - Polling idle state
+ *
+ * This file is released under the GPLv2.
  */
 
 #include <linux/cpuidle.h>
@@ -8,6 +9,7 @@
 #include <linux/sched/clock.h>
 #include <linux/sched/idle.h>
 
+#define POLL_IDLE_TIME_LIMIT	(TICK_NSEC / 16)
 #define POLL_IDLE_RELAX_COUNT	200
 
 static int __cpuidle poll_idle(struct cpuidle_device *dev,
@@ -19,7 +21,6 @@ static int __cpuidle poll_idle(struct cpuidle_device *dev,
 
 	local_irq_enable();
 	if (!current_set_polling_and_test()) {
-		u64 limit = (u64)drv->states[1].target_residency * NSEC_PER_USEC;
 		unsigned int loop_count = 0;
 
 		while (!need_resched()) {
@@ -28,7 +29,7 @@ static int __cpuidle poll_idle(struct cpuidle_device *dev,
 				continue;
 
 			loop_count = 0;
-			if (local_clock() - time_start > limit) {
+			if (local_clock() - time_start > POLL_IDLE_TIME_LIMIT) {
 				dev->poll_time_limit = true;
 				break;
 			}
