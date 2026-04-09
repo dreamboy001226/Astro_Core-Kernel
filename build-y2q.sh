ae2665a4bbd9e2b41b48705b82f5d4ab96fd1abf
dreamboy001226 <dreamboy001226@gmail.com>
2026-04-09 21:19:51 +0500

    Update build-y2q.sh

--- build-y2q.sh
+++ build-y2q.sh
@@ -8,11 +8,11 @@ NC='\033[0m'
 
 
 # ===== AnyKernel3 =====
-AK3_REPO="https://github.com/skye-tachyon/AnyKernel3"
+AK3_REPO="https://github.com/dreamboy001226/AnyKernel3.git"
 AK3_BRANCH="y2q"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 
-ZIPNAME="not-CI-$(date '+%Y%m%d').zip"
+ZIPNAME="Astro_Core-$(date '+%Y%m%d').zip"
 TC_DIR="$(pwd)/tc/clang-r522817"
 DEFCONFIG="vendor/kona-not_defconfig vendor/samsung/kona-sec-not.config vendor/samsung/y2q.config"
 
